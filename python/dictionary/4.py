a27bf7157a97fb8f3ecb08dbecd92a966117e589
Kushagra Singh <kushagra.singh28@yahoo.com>
2021-09-12 22:18:46 -0700

    python codes

---
+++ python/dictionary/4.py
@@ -0,0 +1,7 @@
+#Iterate a dcitionary using dict.items
+#items() method is used to return the list with all dictionary keys with values.
+
+dict1 = {1:"Kushagra1",2:"Kushagra2",3:"Kushagra3"}
+
+for key,value in dict1.items():
+    print(key,value)
